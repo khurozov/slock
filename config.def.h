be109a42993edbe2bbfdab6252a8a74e8603a9a7
Ilyos Khurozov <ilyos@khurozov.uz>
2023-10-09 17:47:46 +0500

    patches

--- config.def.h
+++ config.def.h
@@ -1,12 +1,17 @@
-/* user and group to drop privileges to */
-static const char *user  = "nobody";
-static const char *group = "nogroup";
-
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
+	[INIT] =   "#000000",   /* after initialization */
+	[INPUT] =  "#001122",   /* during input */
+	[FAILED] = "#220011",   /* wrong password */
 };
 
-/* treat a cleared input like a wrong password (color) */
-static const int failonclear = 1;
+/* do not treat a cleared input like a wrong password (color) */
+static const int failonclear = 0;
+
+/* default message */
+static const char * message = "Enter password";
+
+/* text color */
+static const char * text_color = "#ffffff";
+
+/* text size (must be a valid size) */
+static const char * font_name = "6x13";
